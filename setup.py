c5e450bf3a7afc110669dacb8dd021baba2bb0d9
Zachary <zwiesler@jimmy.harvard.edu>
2018-06-06 10:00:05 -0400

    added changelog

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 from setuptools import setup
 setup(name='matchengine',
-      version='0.1.0',
+      version='0.1.1',
       description="clinical trial matching algorithm",
       author="Zachary Zwiesler",
       author_email="zacharyt_zwiesler@dfci.harvard.edu",
